c514aa529d219cf4ea23a03f891c44b932358083
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2022-06-23 00:23:18 +0530

    feat(landing-page): mlai module not found error resolved

--- src/components/HomepageFeatures/Items.js
+++ src/components/HomepageFeatures/Items.js
@@ -2,19 +2,18 @@ import React from 'react'
 
 function Items({id , title , imagesrc , description}) {
   return (
-    <div className="card-component" id={id} >
+    <div className="card-component" key={id} >
       <div className="upper-container">
         <div className="image-container">
-          <img src={imagesrc} alt=""  className='communities-logo'></img>
+          <img src={imagesrc} alt=""   className='communities-logo'></img>
         </div>
       </div>
       <div className="lower-container">
         <h3 className="title">{title}</h3>
         <div className='communities-description'>
-        <p className='description' style={{ }}>
+        <p className='description' >
           {description}
         </p>
-
         </div>
       </div>
     </div>
--- src/components/HomepageFeatures/dataofsociety.js
+++ src/components/HomepageFeatures/dataofsociety.js
@@ -1,20 +1,21 @@
 
-import SDlogo from "../../../static/img/WebDev.png";
+import SDlogo from "../../../static/img/webdev.png";
 import Robotics from "../../../static/img/robotics.png";
-import CP from "../../../static/img/Cp.png";
-import coreElectronics from "../../../static/img/robotics.png";
-import BlockchainCyber from "../../../static/img/BlockchainCyber.png";
-import AIandDS from "../../../static/img/MLAI.png";
+import CP from "../../../static/img/cp.png";
+import coreElectronics from "../../../static/img/elec.png";
+import BlockchainCyber from "../../../static/img/blockchaincyber.png";
+import AIandDS from "../../../static/img/mlai.png";
+
 const dataofsociety = [
   {
-    id: "1",
+    id: 1,
     title: "Community of Software Devlopment",
     description:
       "Web and application development community of IIIT Ranchi has been primarily responsible for providing innovative technical solutions to most of the issues in the institute, The sole purpose of our club is developing web application",
     imagesrc: SDlogo,
   },
   {
-    id: "2",
+    id: 2,
     title: "Community Of Data Science And Artificial Intelligence",
     description:
       "Machine learning community of IIIT Ranchi aims at building and fostering a vibrant machine learning community. Our members gain a unique opportunity to develop machine learning solutions tailored towards relevant and specific industry challenges.",
@@ -22,21 +23,21 @@ const dataofsociety = [
   },
 
   {
-    id: "4",
+    id: 4,
     title: "Community Of Competitive Coding",
     description:
       "Technology has revolutionized the world and holds the key to our future. Department of Computer Science and Engineering, IIIT Ranchi , conducts various technical activities at regular intervals in association with Competitive coding community. ",
     imagesrc: CP,
   },
   {
-    id: "5",
+    id: 5,
     title: "Community Of Core Electronics and IOT",
     description:
       "Electronics and IoT community has the sole objective of inculcating a spirit of developing innovative technology solutions to real life problems. We provide a platform to the students’ body where any individual with an idea can approach the club freely  ",
     imagesrc: coreElectronics,
   },
   {
-    id: "6",
+    id: 6,
     title: "Community Of Robotics And Automation",
     description:
       "Does that image of R2D2 inspire you to create a personal one for yourself? If you have ever even secretly aspired to build your own robot, then the Robotics Community, IIIT Ranchi is the place for you! One on one guidance, workshops and tutorials along with tools ",
@@ -44,7 +45,7 @@ const dataofsociety = [
   },
   
   {
-    id: "3",
+    id: 3,
     title: "Community Of Cybersecurity And BlockChain",
     description:
       "Blockchain and Cybersecurity community has been focusing on the concepts that has been revolutionizing the modern tech world. We are newly established and responsible for walking students through the path of security and innovation.",
