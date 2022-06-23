f27bf0863532896b133ad8507fefd2a8979d462c
kaustubh-s1 <shuklakaustubh84@gmail.com>
2022-06-24 00:01:33 +0530

    fix(ico): fix favicon error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/hg-light.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
